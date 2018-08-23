4af6fdb499bdbb70f73b2728f279436e04ca8805
Gonzalo Castillo <gonzalo.castillo@continuum.cl>
2018-08-22 22:16:15 -0300

    Add HttpChannelException and RestClientException

---
+++ src/main/java/cl/qvo/net/http/exception/RestClientException.java
@@ -0,0 +1,22 @@
+package cl.qvo.net.http.exception;
+
+public class RestClientException extends Exception {
+    public RestClientException() {
+    }
+
+    public RestClientException(String message) {
+        super(message);
+    }
+
+    public RestClientException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public RestClientException(Throwable cause) {
+        super(cause);
+    }
+
+    public RestClientException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
