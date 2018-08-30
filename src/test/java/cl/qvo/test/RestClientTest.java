2c80be62220b54052687302d7d09b40246ddfae5
Gonzalo Castillo <gonzalo.castillo@continuum.cl>
2018-08-30 19:48:23 -0300

    Delete unused imports

--- src/test/java/cl/qvo/test/RestClientTest.java
+++ src/test/java/cl/qvo/test/RestClientTest.java
@@ -4,16 +4,12 @@ import cl.qvo.net.http.RestClient;
 import cl.qvo.net.http.RestClientImpl;
 import cl.qvo.net.http.exception.RestException;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.powermock.modules.junit4.PowerMockRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static junit.framework.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
-
-@RunWith(PowerMockRunner.class)
 public class RestClientTest {
     private static Logger log = LoggerFactory.getLogger(RestClientTest.class);
 
